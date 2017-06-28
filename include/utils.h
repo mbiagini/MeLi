5a5cb12dd1570436fc906638cc809d3b9170fc86
osboxes.org <nmarcantonio@itba.edu.ar>
2017-06-28 04:31:54 +0100

    removed duplicated

--- include/utils.h
+++ include/utils.h
@@ -2,10 +2,7 @@
 #define _utils_h
 #include "types.h"
 
-var_type validExpr(expresion e1,expresion e2);
-var_type validAddExpr(expresion e1,expresion e2);
-int  validate(char * type, var_type expr);
-int countCharInString(char * str, char c);
+
 int intLength(int num);
 
 char *substr(char *str, int start, int end);
